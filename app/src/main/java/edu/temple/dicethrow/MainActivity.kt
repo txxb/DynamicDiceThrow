4fe62785a89758ed4019f743bf2d3ece1c185d15
Maab Taha <tmaab13@gmail.com>
2025-03-26 15:56:58 -0400

    view appears based on orientation

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,11 +1,7 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 /*
 Our DieThrow application has been refactored to move the dieRoll() logic
@@ -23,20 +19,25 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* TODO 1: Load fragment(s)
-            - Show _only_ ButtonFragment if portrait
-            - show _both_ fragments if Landscape
-          */
-    }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, ButtonFragment())
+                .commit()
 
+            if (resources.configuration.orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+                supportFragmentManager.beginTransaction().replace(R.id.container1, ButtonFragment()).replace(R.id.container2, DieFragment()).commit()
+            }
+        }
+
+    }
     /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
         */
 
     // This callback function gets invoked when the child Fragment invokes it
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
-
+        if (resources.configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT) {
+            supportFragmentManager.beginTransaction().replace(R.id.container1, DieFragment()).addToBackStack(null).commit()
+        }
     }
-
-
 }
